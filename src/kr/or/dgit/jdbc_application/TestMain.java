71adc4ba11553b02db9f2d17f66997be2f3d60e9
HyunChul Lee <hc89@naver.com>
2017-10-11 14:05:58 +0900

    jdbc_application 초기화 작업 및 DB연결 테스트 완료

---
+++ src/kr/or/dgit/jdbc_application/TestMain.java
@@ -0,0 +1,17 @@
+package kr.or.dgit.jdbc_application;
+
+import java.sql.Connection;
+
+import kr.or.dgit.jdbc_application.jdbc.DBCon;
+import kr.or.dgit.jdbc_application.jdbc.jdbcUtil;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		DBCon dbCon = DBCon.getInstance();
+		Connection connection = dbCon.getConnection();
+		System.out.println(connection);
+		
+		jdbcUtil.close(connection);
+	}
+}
